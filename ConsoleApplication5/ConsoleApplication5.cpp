f613b65cfe87568b9e4d4f8d17dac76a5f0734f0
IvanDt03 <vanya2003569@yandex.ru>
2023-03-20 21:09:35 +0300

    Изменен текст вывода.

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -6,13 +6,13 @@ int main()
 	int a = 5;
 	std::cout << a;
 	std::cout << std::endl;
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Hallo World" << std::endl;
 
 	foo(a);
 
 	std::cout << a;
 	std::cout << std::endl;
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Hallo" << std::endl;
 
 	foo(a);
 
