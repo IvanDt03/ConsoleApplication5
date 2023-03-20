edc0aa475740cabff5097c05332659556e0bb2b4
IvanDt03 <vanya2003569@yandex.ru>
2023-03-20 21:14:05 +0300

    Улучшили

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -4,17 +4,18 @@
 int main()
 {
 	int a = 5;
+	const int b = 3;
 	std::cout << a;
 	std::cout << std::endl;
 	std::cout << "Hallo World" << std::endl;
 
-	foo(a);
+	foo(a, b);
 
-	std::cout << a;
+	std::cout << a << b;
 	std::cout << std::endl;
 	std::cout << "Hallo" << std::endl;
 
-	foo(a);
+	foo(a, b);
 
 
 }
--- ConsoleApplication5/Header.h
+++ ConsoleApplication5/Header.h
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void foo(int a) {
+void foo(int a, const int b) {
 	for (int i = 0; i < a; ++i) {
 		cout << "Hi  ";
 	}
